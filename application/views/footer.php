1789e459f9865900ac89e68840155137265a6d12
Steven-Soult-Jr <stevensoultjr@gmail.com>
2017-10-30 22:43:00 -0700

    Split home file into 3, header/home_view/footer. Added popper.js so the dropdown now works. Removed the user guide since it is kinda useless (available online). Also changed the files to min versions to save space, since we are unlikely to be editing them.

---
+++ application/views/footer.php
@@ -0,0 +1,12 @@
+    <footer class="container">
+      <p>&copy; Steven 2017</p>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../../assets/js/jquery.min.js"></script>
+    <script src="../../assets/js/popper.min.js"></script>
+    <script src="../../assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
